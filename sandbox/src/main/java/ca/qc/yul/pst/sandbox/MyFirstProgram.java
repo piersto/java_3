9f34482e99383a9b3d5a36bf1473f28d9fb4ccd6
piersto <44554156+piersto@users.noreply.github.com>
2021-04-18 15:40:46 -0400

    Added some functions

--- sandbox/src/main/java/ca/qc/yul/pst/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ca/qc/yul/pst/sandbox/MyFirstProgram.java
@@ -2,7 +2,26 @@ package ca.qc.yul.pst.sandbox;
 
 public class MyFirstProgram {
 
-public static void main(String[] args) {
-System.out.println("Hello, world!");
-}
+    public static void main(String[] args) {
+        hello("world");
+        hello("Vasia");
+        double len = 5;
+        double width = 6;
+
+        System.out.println("Sqaare of the rectangular with side " + len + " equals to " + area(len) );
+        System.out.println("Square of the rectangular with sides "
+                + len + " and " + width + " equals to " + square(len, width));
+
+    }
+    public static void hello(String s) {
+        System.out.println("Hello, " + s + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double square (double l, double w) {
+        return l * w;
+    }
 }
