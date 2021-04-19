afb0ff58be416fb038ae986ac39f5075120a9cab
piersto <44554156+piersto@users.noreply.github.com>
2021-04-19 14:17:18 -0400

    Лекция 2.4. Рефакторинг: улучшаем читаемость кода, выделяем вспомогательные методы Done

--- addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/GroupCreationTests.java
@@ -19,15 +19,15 @@ public class GroupCreationTests {
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
-        login();
+        login("admin", "secret");
     }
 
-    private void login() {
+    private void login(String username, String password) {
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.id("LoginForm")).click();
         wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
@@ -37,7 +37,7 @@ public class GroupCreationTests {
 
         goToGroupPage();
         initGroupCreation();
-        fillGroupForm();
+        fillGroupForm(new GroupData("Group", "group header", "Group footer"));
         submitGroupCreation();
         returnToGroupPage();
     }
@@ -50,16 +50,16 @@ public class GroupCreationTests {
         wd.findElement(By.name("submit")).click();
     }
 
-    private void fillGroupForm() {
+    private void fillGroupForm(GroupData groupData) {
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("Group");
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("group header");
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("Group footer");
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
     }
 
     private void initGroupCreation() {
