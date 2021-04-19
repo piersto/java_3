6ca395a7ee657750e920de9b50fbd13cc051fe15
piersto <44554156+piersto@users.noreply.github.com>
2021-04-18 23:51:32 -0400

    Лекция 2.3. Фикстуры: инициализация и зачистка Extracted login to BeforeMethod

--- addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,12 +17,8 @@ public class GroupCreationTests {
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
-        wd = new FirefoxDriver();
+        wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
-
-    @Test
-    public void testGroupCreation() throws Exception {
         wd.get("http://localhost/addressbook/");
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
@@ -30,6 +27,11 @@ public class GroupCreationTests {
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.id("LoginForm")).click();
         wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    @Test
+    public void testGroupCreation() throws Exception {
+
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
