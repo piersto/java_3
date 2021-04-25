f1d7c91fe7f6b4ca5a66eba87c53f084a1238283
piersto <44554156+piersto@users.noreply.github.com>
2021-04-25 13:57:47 -0400

    Лекция 2.5. Создаём общий базовый класс для тестов, где можно хранить методы, общие для всех тестов Replaced inheritance with delegations -- added ApplicationManager

--- addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/TestBase.java
@@ -10,112 +10,17 @@ import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestBase {
-
-    public WebDriver wd;
+public class TestBase extends ApplicationManager {
 
     @BeforeMethod(alwaysRun = true)
 
     public void setUp() throws Exception {
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
-        login("admin", "secret");
-    }
-
-    protected void login(String username, String password) {
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.id("LoginForm")).click();
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
-
-    protected void returnToGroupPage() {
-        wd.findElement(By.linkText("group page")).click();
-    }
-
-    protected void submitGroupCreation() {
-        wd.findElement(By.name("submit")).click();
-    }
-
-    protected void fillGroupForm(GroupData groupData) {
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
-
-    protected void initGroupCreation() {
-        wd.findElement(By.name("new")).click();
-    }
-
-    protected void goToGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        init();
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
-        wd.quit();
+        stop();
     }
 
-    protected boolean isElementPresent(By by) {
-        try {
-            wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    protected boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    protected void returnToHomePage() {
-      wd.findElement(By.linkText("home")).click();
-    }
-
-    protected void deleteSelectedGroups() {
-      wd.findElement(By.name("delete")).click();
-    }
-
-    protected void selectGroup() {
-      wd.findElement(By.name("selected[]")).click();
-    }
-
-    protected void deleteContacts() {
-      wd.findElement(By.xpath("//input[@value='Delete']")).click();
-      wd.switchTo().alert().accept();
-    }
-
-    protected void selectContact() {
-      wd.findElement(By.name("selected[]")).click();
-    }
-
-    protected void submitContactCreation() {
-        wd.findElement(By.name("submit")).click();
-    }
-
-    protected void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-    }
-
-    protected void initContactCreation() {
-        wd.findElement(By.cssSelector("li.all a")).click();
-    }
 }
