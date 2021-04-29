2fd8cee5adf541d8679b76fddd7dda05b5df0131
piersto <44554156+piersto@users.noreply.github.com>
2021-04-28 21:36:32 -0400

    Лекция 3.3. Запуск тестов в разных браузерах Browser is specified from TestBase

--- addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/appmanager/ApplicationManager.java
@@ -2,6 +2,10 @@ package ca.qc.yul.pst.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,10 +16,25 @@ public class ApplicationManager {
     private SessionHelper sessionHelper;
     public NavigationHelper navigationHelper;
     public GroupHelper groupHelper;
+    private String browser;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new ChromeDriver();
+        if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        }
+        else if (browser == BrowserType.FIREFOX) {
+            wd = new FirefoxDriver();
+        }
+        else if(browser == BrowserType.EDGE) {
+            wd = new EdgeDriver();
+        }
+        else if(browser == BrowserType.IE) {
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/TestBase.java
@@ -1,15 +1,16 @@
 package ca.qc.yul.pst.addressbook.tests;
 
 import ca.qc.yul.pst.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    // Here we change browser if needed!!!
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.EDGE);
 
     @BeforeMethod(alwaysRun = true)
-
     public void setUp() throws Exception {
         app.init();
     }
