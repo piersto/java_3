a484b6622db6e6826092b0d620b73ed6a7a295e0
piersto <44554156+piersto@users.noreply.github.com>
2021-04-30 22:28:16 -0400

    Лекция 3.9. "Ленивые" действия Added verification, if should go to the page or is already on it

--- addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/appmanager/NavigationHelper.java
@@ -3,6 +3,8 @@ package ca.qc.yul.pst.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.Objects;
+
 public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(WebDriver wd) {
@@ -10,10 +12,19 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void goToGroupPage() {
+        if (elementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && elementPresent(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void returnToHomePage() {
-      click(By.linkText("home"));
+        if (elementPresent(By.cssSelector("span#search_count"))
+        && wd.findElement(By.cssSelector("label strong")).getText().equals("Number of results: ")) {
+            return;
+        }
+        click(By.linkText("home"));
     }
 }
