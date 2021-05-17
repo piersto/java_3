292a353fb79ccaef4d90406445ecf038c4e05e4e
piersto <44554156+piersto@users.noreply.github.com>
2021-05-16 22:32:44 -0400

    Лекция 4.5. Формирование коллекции Done for groups

--- addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/GroupDeletionTests.java
@@ -4,21 +4,23 @@ import ca.qc.yul.pst.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.List;
+
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().goToGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup
               (new GroupData("Group to be deleted", "Header", "Footer"));
     }
+    List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(0);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
+    List<GroupData> after = app.getGroupHelper().getGroupList();
     app.getNavigationHelper().returnToHomePage();
-    Assert.assertEquals(before - 1, after);
+    Assert.assertEquals(before.size() - 1, after.size());
   }
 }
--- addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/GroupModificationTests.java
@@ -4,23 +4,25 @@ import ca.qc.yul.pst.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().goToGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup
                     (new GroupData("Group to be modified", "Header", "Footer"));
         }
-        app.getGroupHelper().selectGroup(before - 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("New Group"
-                , "New header", "New footer"));
+        app.getGroupHelper().fillGroupForm(
+                new GroupData("New Group", "New header", "New footer"));
         app.getGroupHelper().submitGroupModification();
         app.navigationHelper.goToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(before, after);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(before.size(), after.size());
     }
 }
