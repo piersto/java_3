7bbf5cc211d404101c50f4d99ce3c9be396a63a4
piersto <44554156+piersto@users.noreply.github.com>
2021-05-18 23:06:35 -0400

    Лекция 4.6. Сравнение коллекций Generated ToString, Equals, hashCode for ContactData

--- addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/model/ContactData.java
@@ -1,10 +1,33 @@
 package ca.qc.yul.pst.addressbook.model;
 
+import java.util.Objects;
+
 public class ContactData {
 
     private final String lastName;
     private final String firstName;
     private String group;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactData that = (ContactData) o;
+        return Objects.equals(lastName, that.lastName) && Objects.equals(firstName, that.firstName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lastName, firstName);
+    }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                '}';
+    }
 
     public ContactData(String lastName, String firstName, String group) {
         this.lastName = lastName;
