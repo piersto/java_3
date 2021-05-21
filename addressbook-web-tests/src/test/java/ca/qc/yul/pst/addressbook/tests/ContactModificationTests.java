657a59d07ad724ff305965a65aae5a27d95ae3c3
piersto <44554156+piersto@users.noreply.github.com>
2021-05-20 20:22:35 -0400

    Лекция 4.7. Множества: неупорядоченные коллекции Started fo ContactModificationTests

--- addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/ContactModificationTests.java
@@ -4,6 +4,7 @@ import ca.qc.yul.pst.addressbook.model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactModificationTests extends TestBase {
@@ -16,12 +17,17 @@ public class ContactModificationTests extends TestBase {
             app.getNavigationHelper().returnToHomePage();
         }
         List<ContactData> before = app.getContactHelper().getContactList();
+        ContactData contact = new ContactData("Ivanov", "Vasyl", null);
         app.getContactHelper().initContactModification(0);
         app.getContactHelper()
-                .fillContactForm(new ContactData("Ivanov", "Vasyl", null), false);
+                .fillContactForm(contact, false);
         app.getContactHelper().submitContactModification();
         app.getNavigationHelper().returnToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(before.size(), after.size());
+
+        before.remove(0);
+        before.add(contact);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
