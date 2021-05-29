5a99b455b35d64d5a9a391cff8a6f6846984226a
piersto <44554156+piersto@users.noreply.github.com>
2021-05-29 12:24:11 -0400

    Лекция 4.10. Сортировка списков Done GroupModificationTests

--- addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import ca.qc.yul.pst.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -28,6 +29,9 @@ public class GroupModificationTests extends TestBase {
 
         before.remove(before.size() - 1);
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
