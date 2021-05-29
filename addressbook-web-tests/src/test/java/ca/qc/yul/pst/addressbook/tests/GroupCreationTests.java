b7d247ff9e4d736c0461f9c61f3928c58799f3ef
piersto <44554156+piersto@users.noreply.github.com>
2021-05-29 12:15:02 -0400

    Лекция 4.9. Лямбда-выражения (анонимные функции) и элементы функционального программирования Done for GroupCreationTests

--- addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import ca.qc.yul.pst.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -18,15 +19,17 @@ public class GroupCreationTests extends TestBase {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         app.getNavigationHelper().goToGroupPage();
         Assert.assertEquals(before.size() + 1, after.size());
+/*
         int max = 0;
         for (GroupData g : after) {
             if (g.getId() > max) {
                 max = g.getId();
             }
-        group.setId(max);
         }
+
+ */
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
-
     }
 }
