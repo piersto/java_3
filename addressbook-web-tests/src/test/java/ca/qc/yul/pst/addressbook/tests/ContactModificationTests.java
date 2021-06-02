df30518a231ed487b6a2e34d7eff0000909681fb
piersto <44554156+piersto@users.noreply.github.com>
2021-06-01 23:06:37 -0400

    Лекция 4.10. Сортировка списков Done for ContactModificationTests

--- addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ca/qc/yul/pst/addressbook/tests/ContactModificationTests.java
@@ -4,6 +4,7 @@ import ca.qc.yul.pst.addressbook.model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -29,6 +30,10 @@ public class ContactModificationTests extends TestBase {
 
         before.remove(0);
         before.add(contact);
+        Comparator<? super ContactData> byId = (c1, c2) ->
+                Integer.compare(c1.getId(), c2.getId());
+        before.sort(byId);
+        after.sort(byId);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
